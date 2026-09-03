f4b77288cd03273077ad6703e7fe99d33f9c4c93
peter-duncanson <pduncanson@gmail.com>
2026-09-02 21:33:37 -0400

    completed prime factors

--- c/src/prime_factors.c
+++ c/src/prime_factors.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stddef.h>
 #include <string.h>
 
 /*
@@ -16,56 +15,31 @@ Example: n = 86240 should return "(2**5)(5)(7**2)(11)"
 
 char *factors(int n)
 {
-    size_t array_size = 100;
-    size_t *primes = calloc(array_size, sizeof(size_t));
-    size_t *prime_counts = calloc(array_size, sizeof(size_t));
-    size_t div = 2, i = 0, count = 0;
-    char *sol = malloc(array_size * sizeof(char)), buffer[100];
-    
-    while (primes[0] == 0)
+    char *sol = calloc(100, sizeof(char));
+    char buffer[100];
+
+    for (int div = 2; div * div <= n; div++)
     {
-        if (n % div == 0)
+        if (n % div != 0) continue;
+
+        int count = 0;
+
+        while (n % div == 0)
         {
-            primes[i++] = div;
+            n /= div;
+            count++;
         }
-        else div++;
+
+        if (count == 1) snprintf(buffer, sizeof(buffer), "(%d)", div);
+        else snprintf(buffer, sizeof(buffer), "(%d**%d)", div, count);
+
+        strcat(sol, buffer);
     }
 
-    while (n > 1)
+    if (n > 1)
     {
-        if (n % div == 0)
-        {
-            if (div != primes[i - 1])
-            {
-                primes[i] = div;
-                prime_counts[i - 1] = count;
-                count = 0;
-                i++;
-            }
-            else
-            {
-                ++count;
-                n /= div;
-            }
-        }
-        else div++;
-    }
-
-    for (size_t k = 0; k < i; k++)
-    {
-        if (prime_counts[k] == 0) prime_counts[k] = 1;
-        if (prime_counts[k] == 1)
-        {
-            int len = snprintf(buffer, (size_t) 99, "(%ld)", primes[k]);
-            sol = strncat(sol, buffer, (size_t) len); 
-        }
-
-        else
-        {
-            int len = snprintf(buffer, (size_t) 99, "(%ld**%ld)", primes[k], prime_counts[k]);
-            sol = strncat(sol, buffer, (size_t) len); 
-        }
-
+        snprintf(buffer, sizeof(buffer), "(%d)", n);
+        strcat(sol, buffer);
     }
 
     return sol;
@@ -73,12 +47,6 @@ char *factors(int n)
 
 int main(void)
 {
-    char *a = factors(2);
-    char *b = factors(435480599);
-    char *c = factors(86240);
-    printf("%s\n\n", a);
-    printf("%s\n\n", b);
-    printf("%s\n\n", c);
 
     return 0;
 }
